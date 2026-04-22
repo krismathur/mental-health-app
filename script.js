e9feb2c34f7de935963003bb271420122a4aa11c
Krish Mathur <krishmathur1001@gmail.com>
2026-04-21 18:22:26 -0700

    js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const button = document.getElementById("submitBtn");
+button.addEventListener("click", function (event){
+    event.preventDefault();
+    const name = document.getElementById("name");
+    const age = document.getElementById("age");
+    const sport = document.getElementById("sport");
+
+
+    })
